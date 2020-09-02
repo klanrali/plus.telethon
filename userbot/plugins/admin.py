8c557bee9f05e879a16313d508f00182973b6a6f
i.q <64720410+klanrali@users.noreply.github.com>
2020-09-02 23:04:09 +0300

    i.q-admins.py

--- userbot/plugins/admin.py
+++ userbot/plugins/admin.py
@@ -1,9 +1,8 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-"""
-Userbot module to help you manage a group
+#Hello. These files are all private to Source Iraq Thun. 
+#In short, there are files registered for Source, another group. 
+#You do not need to write a file from the beginning for the sake of rights, 
+#and there are complete files. Thank you for installing Iraq Thun. 
+#Our channel is here: https://t.me/tele_thonp
 """
 
 from asyncio import sleep
