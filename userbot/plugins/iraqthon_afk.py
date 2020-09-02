688efac0607f0fe5220b223700b450ebfc63e729
i.q <64720410+klanrali@users.noreply.github.com>
2020-09-02 23:31:40 +0300

    Update and rename afk.py to iraqthon_afk.py

--- userbot/plugins/iraqthon_afk.py
+++ userbot/plugins/iraqthon_afk.py
@@ -1,5 +1,10 @@
-"""AFK Plugin for @UniBorg
-Syntax: .afk REASON"""
+#Hello. These files are all private to Source Iraq Thun. 
+#In short, there are files registered for Source, another group. 
+#You do not need to write a file from the beginning for the sake of rights, 
+#and there are complete files. Thank you for installing Iraq Thun. 
+#Our channel is here: https://t.me/tele_thonp
+#Syntax: .afk REASON"""
+
 import asyncio
 import datetime
 from datetime import datetime
