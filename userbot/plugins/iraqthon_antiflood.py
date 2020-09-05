a364d7a0d631ecd5061e2250f21f402ff9b1938b
i.q <64720410+klanrali@users.noreply.github.com>
2020-09-05 13:13:48 +0300

    Update and rename antiflood.py to iraqthon_antiflood.py

--- userbot/plugins/iraqthon_antiflood.py
+++ userbot/plugins/iraqthon_antiflood.py
@@ -1,3 +1,9 @@
+#Hello. These files are all private to Source Iraq Thun. 
+#In short, there are files registered for Source, another group. 
+#You do not need to write a file from the beginning for the sake of rights, 
+#and there are complete files. Thank you for installing Iraq Thun. 
+#Our channel is here: https://t.me/tele_thon
+
 import asyncio
 from .. import CMD_HELP
 from telethon import events
