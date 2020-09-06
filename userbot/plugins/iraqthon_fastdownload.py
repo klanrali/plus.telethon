550c5315c9ac01b6b2f8e871243ce126334348d7
i.q <64720410+klanrali@users.noreply.github.com>
2020-09-06 14:07:40 +0300

    Update and rename fastdownload.py to iraqthon_fastdownload.py

--- userbot/plugins/iraqthon_fastdownload.py
+++ userbot/plugins/iraqthon_fastdownload.py
@@ -1,6 +1,9 @@
-"""
-command: .url 
-"""
+#Hello. These files are all private to Source Iraq Thun. 
+#In short, there are files registered for Source, another group. 
+#You do not need to write a file from the beginning for the sake of rights, 
+#and there are complete files. Thank you for installing Iraq Thun. 
+#Our channel is here: https://t.me/tele_thon
+
 import aria2p
 from telethon import events
 import asyncio
