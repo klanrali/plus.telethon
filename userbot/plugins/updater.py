4b8138961456d8bda2415b451ae731723c75a115
i.q <64720410+klanrali@users.noreply.github.com>
2020-09-10 23:50:52 +0300

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -19,7 +19,7 @@ from userbot.utils import admin_cmd
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 UPSTREAM_REPO_BRANCH = "master"
-UPSTREAM_REPO_URL = "https://github.com/sandy1709/catuserbot"
+UPSTREAM_REPO_URL = "https://github.com/klanrali/iraqthon"
 
 requirements_path = path.join(
     path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
@@ -115,10 +115,10 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             await asyncio.sleep(5)
             return await event.delete()
         else:
-            await event.edit("`Successfully deployed!\n" "Restarting, please wait...`")
+            await event.edit("`Successfully deployed! @iraqthon\n" "Restarting, please wait...`")
     else:
         await event.edit(
-            "`[HEROKU]`\n" "`Please set up`  **HEROKU_API_KEY**  `variable...`"
+            "`[HEROKU]`\n" "`Please set up @iraqthon`  **HEROKU_API_KEY**  `variable...`"
         )
     return
 
@@ -129,7 +129,7 @@ async def update(event, repo, ups_rem, ac_br):
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
     await event.edit(
-        "`Successfully Updated!\n" "Bot is restarting... Wait for a second!`"
+        "`Successfully Updated!\n" "Bot is restarting... Wait for a second! @iraqthon`"
     )
     # Spin a new instance of bot
     args = [sys.executable, "-m", "userbot"]
