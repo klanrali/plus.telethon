58be0ac33b0a9b7b99ee34efa477006bfd433ae3
i.q <64720410+klanrali@users.noreply.github.com>
2020-09-10 20:00:13 +0300

    Update iraqthon_alive.py

--- userbot/plugins/iraqthon_alive.py
+++ userbot/plugins/iraqthon_alive.py
@@ -20,7 +20,7 @@ from userbot import CMD_HELP, ALIVE_NAME, catdef , catversion
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 CAT_IMG = Config.ALIVE_PIC
 
-@borg.on(admin_cmd(outgoing=True, pattern="aliv$"))
+@borg.on(admin_cmd(outgoing=True, pattern="alive$"))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
